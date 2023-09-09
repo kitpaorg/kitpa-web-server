4d1f10862e79abb26e10989db2753649453b465f
Hank <77097180+hankbae93@users.noreply.github.com>
2023-09-09 20:05:03 +0900

    Exam, Problem 페이지 업데이트 (#1)
    * update: login html
    
    * update: problem, exam, login

--- src/main/java/com/kitpa/kitpaserver/config/SecurityConfig.java
+++ src/main/java/com/kitpa/kitpaserver/config/SecurityConfig.java
@@ -80,7 +80,8 @@ public class SecurityConfig {
                 .authenticationProvider(userAuthenticationProvider)
                 .antMatcher("/**")
                 .authorizeHttpRequests(re ->
-                        re.anyRequest().hasRole("USER")
+                        re.antMatchers("/css/**").permitAll()
+                        .anyRequest().hasRole("USER")
                 )
                 .formLogin(form ->
                         form
